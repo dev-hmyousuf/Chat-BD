a554eacebb9ea7716192d84acd2e002e4e5a1f56
betomoedano <betomoedano@outlook.com>
2025-03-13 19:41:34 -0500

    set dark theme / and layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
